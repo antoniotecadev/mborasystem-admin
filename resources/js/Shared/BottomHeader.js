22dcaa7e4b7e4ef17ea28c03db0950ac4c9aff4b
António Teca <antonioteca@hotmail.com>
2022-01-30 20:50:48 +0100

    icone notificação

--- resources/js/Shared/BottomHeader.js
+++ resources/js/Shared/BottomHeader.js
@@ -2,14 +2,54 @@ import React, { useState } from 'react';
 import { InertiaLink, usePage } from '@inertiajs/inertia-react';
 import Icon from '@/Shared/Icon';
 import logo from '@/img/logotipo-yoga-original.png';
+import { borderRadius } from 'tailwindcss/defaultTheme';
 
 export default () => {
   const { auth } = usePage().props;
   const [menuOpened, setMenuOpened] = useState(false);
+  const [notificationOpened, setNotificationOpened] = useState(false);
   return (
     <div className="flex items-center justify-between w-full p-4 text-sm bg-white border-b md:py-0 md:px-12 d:text-md">
       <div><img className="text-white fill-current" width="120" height="28" src={`./${logo}`} alt='sem foto'/></div>
       <div className="mt-1 mr-4 font-bold">{auth.user.account.name}</div>
+      <div className="relative">
+        <div
+          className="flex items-center cursor-pointer select-none group"
+          onClick={() => setNotificationOpened(true)}
+        >
+          <Icon
+            className="w-5 h-5 text-gray-800 fill-current group-hover:text-indigo-600 focus:text-indigo-600"
+            name="notificacao"
+          />
+          <div className="absolute bg-indigo-100 text-white whitespace-nowrap group-hover:text-indigo-600 focus:text-indigo-600 mb-6" style={{backgroundColor: 'red', paddingRight: '5px', borderRadius: '10px'}}>
+            <span className="ml-1">1</span>
+          </div>
+        </div>
+        <div className={notificationOpened ? '' : 'hidden'}>
+          <div className="absolute top-0 right-0 left-auto z-20 py-2 mt-8 text-sm whitespace-nowrap bg-white rounded shadow-xl">
+            <InertiaLink
+              href={route('users.edit', auth.user.id)}
+              className="block px-6 py-2 hover:bg-indigo-600 hover:text-white"
+              onClick={() => setMenuOpened(false)}
+            >
+              Registo
+            </InertiaLink>
+            <InertiaLink
+              href={route('users')}
+              className="block px-6 py-2 hover:bg-indigo-600 hover:text-white"
+              onClick={() => setMenuOpened(false)}
+            >
+              Pagamento
+            </InertiaLink>
+          </div>
+          <div
+            onClick={() => {
+              setNotificationOpened(false);
+            }}
+            className="fixed inset-0 z-10 bg-black opacity-25"
+          ></div>
+        </div>
+      </div>
       <div className="relative">
         <div
           className="flex items-center cursor-pointer select-none group"
