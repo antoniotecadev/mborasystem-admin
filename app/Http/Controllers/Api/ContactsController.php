652aed82644916ffc08a136244cf2fd69fe06a2d
António Teca <antonioteca@hotmail.com>
2022-02-03 14:19:18 +0100

    sem pagamento do serviço retorno de dados vazios

--- app/Http/Controllers/Api/ContactsController.php
+++ app/Http/Controllers/Api/ContactsController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\DB;
 class ContactsController extends Controller
 {
     public function index($imei){
-        
+
         $c = DB::table('contacts')
         ->join('pagamentos', 'pagamentos.contact_id', '=', 'contacts.id')
         ->where('imei', $imei)
@@ -21,33 +21,51 @@ class ContactsController extends Controller
         ->select('contacts.first_name', 'contacts.last_name', 'contacts.nif_bi', 'contacts.email', 'contacts.phone', 'contacts.alternative_phone', 'contacts.cantina', 'contacts.municipality', 'contacts.district', 'contacts.street', 'contacts.estado', 'contacts.imei', 'pagamentos.pacote', 'pagamentos.inicio', 'pagamentos.fim')
         ->limit(1)
         ->get();
-        
+
         // $dataFim = new DateTime($collection['0']->inicio);
         // $dataFim->add(new DateInterval('P30D'));
         // $dataFim->format('Y-m-d');
 
-        if($c['0']->fim <= date('Y-m-d') || $c['0']->estado == 0) {
-            $termina = 1;
+        if(empty($c['0'])) {
+            return [[ 'first_name' => '',
+            'last_name' => '',
+            'nif_bi' => '',
+            'email' => '',
+            'phone' => '',
+            'alternative_phone' => '',
+            'cantina' => '',
+            'municipality' => '',
+            'district' => '',
+            'street' => '',
+            'estado' => 0,
+            'imei' => '',
+            'pacote' => 3,
+            'inicio' => '',
+            'fim' => '',
+            'termina' => '1' ]];
         } else {
-            $termina = 0;
+            if($c['0']->fim <= date('Y-m-d') || $c['0']->estado == 0) {
+                $termina = 1;
+            } else {
+                $termina = 0;
+            }
+            return [[ 'first_name' => $c['0']->first_name,
+            'last_name' => $c['0']->last_name,
+            'nif_bi' => $c['0']->nif_bi,
+            'email' => $c['0']->email,
+            'phone' => $c['0']->phone,
+            'alternative_phone' => $c['0']->alternative_phone,
+            'cantina' => $c['0']->cantina,
+            'municipality' => $c['0']->municipality,
+            'district' => $c['0']->district,
+            'street' => $c['0']->street,
+            'estado' => $c['0']->estado,
+            'imei' => $c['0']->imei,
+            'pacote' => $c['0']->pacote,
+            'inicio' => $c['0']->inicio,
+            'fim' => $c['0']->fim,
+            'termina' => $termina ]];
         }
-        
-        return [[ 'first_name' => $c['0']->first_name,
-         'last_name' => $c['0']->last_name, 
-         'nif_bi' => $c['0']->nif_bi, 
-         'email' => $c['0']->email, 
-         'phone' => $c['0']->phone, 
-         'alternative_phone' => $c['0']->alternative_phone, 
-         'cantina' => $c['0']->cantina, 
-         'municipality' => $c['0']->municipality, 
-         'district' => $c['0']->district, 
-         'street' => $c['0']->street, 
-         'estado' => $c['0']->estado, 
-         'imei' => $c['0']->imei, 
-         'pacote' => $c['0']->pacote, 
-         'inicio' => $c['0']->inicio, 
-         'fim' => $c['0']->fim, 
-         'termina' => $termina ]];
 }
 
     public function store(Request $request)
@@ -80,7 +98,7 @@ class ContactsController extends Controller
        }
     }
 
-    public function getBairros() 
+    public function getBairros()
     {
     	return [['mr_bt' => 'Morro Bento']];
     }
