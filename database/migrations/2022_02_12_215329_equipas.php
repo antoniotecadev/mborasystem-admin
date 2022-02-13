3a1215c6a6ff423901e391254a9ac087b4388ef6
António Teca <antonioteca@hotmail.com>
2022-02-13 17:17:00 +0100

    migration equipas

---
+++ database/migrations/2022_02_12_215329_equipas.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Equipas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome_completo', 50);
+            $table->integer('account_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
