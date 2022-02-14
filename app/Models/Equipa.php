ad2c02429b267c78b580b05b9a01a025f2c2cf50
António Teca <antonioteca@hotmail.com>
2022-02-14 13:51:00 +0100

    Equipa preenchida

---
+++ app/Models/Equipa.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Agente;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Equipa extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    public function agentes()
+    {
+        return $this->hasMany(Agente::class);
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->codigo;
+    }
+
+    public function scopeOrderByName($query)
+    {
+        $query->orderBy('codigo');
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('codigo', 'like', '%'.$search.'%');
+            });
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
+}
