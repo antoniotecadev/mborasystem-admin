467b73997119ba27c50885c0063489c2de7ac9d1
António Teca <antonioteca@hotmail.com>
2022-02-17 13:06:29 +0100

    agentes

---
+++ resources/js/Pages/Agentes/Create.js
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink, useForm, usePage } from '@inertiajs/inertia-react';
+import Layout from '@/Shared/Layout';
+import LoadingButton from '@/Shared/LoadingButton';
+import TextInput from '@/Shared/TextInput';
+import SelectInput from '@/Shared/SelectInput';
+import { getDataFimTrimestralSemestralAnual } from '@/Util/utilitario';
+
+const Create = () => {
+  const [datafinal, setDataFinal] = useState();
+  const { contacts } = usePage().props;
+  const { data, setData, errors, post, processing } = useForm({
+    pacote: '',
+    tipo_pagamento: '',
+    inicio: '',
+    fim: '',
+    nome: '',
+    contact_id: ''
+  });
+
+  function diasNoMes(mes, ano) {
+    var data = new Date(ano, mes, 0);
+    return parseInt(data.getDate());
+  }
+
+  function getDataFinal(e) {
+
+    let diaFinal, dataFinal;
+    let data = e.target.value;
+
+    const diaT = parseInt(data.split('-', 3)[2]);
+    const mesT = parseInt(data.split('-', 3)[1]);
+    const anoT = parseInt(data.split('-', 3)[0]);
+
+    const mes = (mesT == 12 ? '' : mesT) + 1;
+    const anoFinal = mesT == 12 ? anoT + 1 : anoT;
+    const mesFinal = (mes < '10' ? '0' : '') + mes;
+    const dia = 30 + diaT - diasNoMes(mesT, anoT);
+
+    if (dia > diasNoMes(mesFinal, anoFinal)) {
+      diaFinal = dia - diasNoMes(mesFinal, anoFinal);
+      dataFinal =
+        anoFinal +
+        '-' +
+        ((parseInt(mesFinal) + 1 < '10' ? '0' : '') +
+          (parseInt(mesFinal) + 1)) +
+        '-' +
+        ((diaFinal < '10' ? '0' : '') + diaFinal);
+    } else {
+      diaFinal =
+        (dia < '10' ? '0' : '') + dia == '00'
+          ? '31'
+          : (dia < '10' ? '0' : '') + dia;
+
+      if (diaT == 1 && diasNoMes(mesT, anoT) == 31) {
+        dataFinal =
+          anoFinal +
+          '-' +
+          ((parseInt(mesFinal) - 1 < '10' ? '0' : '') +
+            (parseInt(mesFinal) - 1)) +
+          '-' +
+          diaFinal;
+      } else {
+        dataFinal = anoFinal + '-' + mesFinal + '-' + diaFinal;
+      }
+    }
+    setData('inicio', e.target.value);
+    setDataFinal(dataFinal);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    post(route('pagamentos.store'));
+  }
+
+  return (
+    <div>
+      <h1 className="mb-8 text-3xl font-bold">
+        <InertiaLink
+          href={route('pagamentos')}
+          className="text-indigo-600 hover:text-indigo-700"
+        >
+          Pagamentos
+        </InertiaLink>
+        <span className="font-medium text-indigo-600"> /</span> Criar
+      </h1>
+      <div className="max-w-3xl overflow-hidden bg-white rounded shadow">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-wrap p-8 -mb-8 -mr-6">
+            <SelectInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Parceiro"
+              name="contact_id"
+              errors={errors.contact_id}
+              value={data.contact_id}
+              onChange={e => setData('contact_id', e.target.value)}
+            >
+              <option value=""></option>
+              {contacts.map(({ id, first_name, last_name, cantina, phone }) => (
+                <option key={id} value={id}>
+                  {first_name} {last_name} - {cantina} - {phone}
+                </option>
+              ))}
+            </SelectInput>
+            <SelectInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Pacote"
+              name="pacote"
+              errors={errors.pacote}
+              value={data.pacote}
+              onChange={e => setData('pacote', e.target.value)}
+            >
+              <option value=""></option>
+              <option value="0">ALUMÍNIO</option>
+              <option value="1">BRONZE</option>
+              <option value="2">OURO</option>
+            </SelectInput>
+            <SelectInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Tipo"
+              name="tipo_pagamento"
+              errors={errors.tipo_pagamento}
+              value={data.tipo_pagamento}
+              onChange={e => setData('tipo_pagamento', e.target.value)}
+            >
+              <option value=""></option>
+              <option value="1">MENSAL</option>
+              <option value="3">TRIMESTRAL</option>
+              <option value="6">SEMESTRAL</option>
+              <option value="12">ANUAL</option>
+            </SelectInput>
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Início"
+              name="inicio"
+              type="date"
+              errors={errors.inicio}
+              value={data.inicio}
+              onChange={e => getDataFinal(e)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              id="fim"
+              label={data.tipo_pagamento == "1"? 'Fim: ' + datafinal : 'Fim: ' + getDataFimTrimestralSemestralAnual(data.inicio, Number(data.tipo_pagamento))}
+              name="fim"
+              type="date"
+              errors={errors.fim}
+              value={data.fim}
+              onChange={e => setData('fim', e.target.value == datafinal || e.target.value == getDataFimTrimestralSemestralAnual(data.inicio, Number(data.tipo_pagamento)) ? e.target.value: '')}
+            />
+          </div>
+          <div className="flex items-center justify-end px-8 py-4 bg-gray-100 border-t border-gray-200">
+            <LoadingButton
+              loading={processing}
+              type="submit"
+              className="btn-indigo"
+            >
+              Efectuar pagamento
+            </LoadingButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+Create.layout = page => <Layout title="Efectuar pagamento" children={page} />;
+
+export default Create;
---
+++ resources/js/Pages/Agentes/Index.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { InertiaLink, usePage } from '@inertiajs/inertia-react';
+import Layout from '@/Shared/Layout';
+import Icon from '@/Shared/Icon';
+import Pagination from '@/Shared/Pagination';
+import SearchFilter from '@/Shared/SearchFilter';
+
+const Index = () => {
+  const { pagamentos } = usePage().props;
+  const {
+    data,
+    meta: { links }
+  } = pagamentos;
+
+  var hoje = new Date();
+
+  var dataActual = hoje.getFullYear() + '-' + String(hoje.getMonth() + 1).padStart(2, '0') + '-' + ((hoje.getDate() < '10' ? '0' : '') + hoje.getDate());
+
+  var amanha = new Date(hoje.getTime());
+  amanha.setDate(amanha.getDate() + 1);
+
+  var dd = amanha.getDate();
+  var mm = amanha.getMonth() + 1;
+  var yyyy = amanha.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  var dataAmanha = yyyy + '-' + mm + '-' + dd;
+
+  return (
+    <div>
+      <h1 className="mb-8 text-3xl font-bold">Pagamentos ({data.length})</h1>
+      <div className="flex items-center justify-between mb-6">
+        <SearchFilter />
+        <InertiaLink
+          className="btn-indigo focus:outline-none"
+          href={route('pagamentos.create')}
+        >
+          <span>Efectuar</span>
+          <span className="hidden md:inline"> Pagamento</span>
+        </InertiaLink>
+      </div>
+      <div className="overflow-x-auto bg-white rounded shadow">
+        <table className="w-full whitespace-nowrap">
+          <thead>
+            <tr className="font-bold text-left">
+              <th className="px-6 pt-5 pb-4">Nome parceiro</th>
+              <th className="px-6 pt-5 pb-4">Cantina</th>
+              <th className="px-6 pt-5 pb-4">Início</th>
+              <th className="px-6 pt-5 pb-4">Fim</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(({ id, inicio, fim, deleted_at, contact }) => (
+              <tr
+                key={id}
+                className={`hover:bg-gray-100 focus-within:bg-gray-100 ${
+                  Date.parse(fim) <= Date.parse(dataActual) ? 'bg-red-100' : Date.parse(fim) == Date.parse(dataAmanha)?'bg-yellow-400':'bg-green-200'
+                }`}
+              >
+                <td className="border-t">
+                  <InertiaLink
+                    href={route('pagamentos.edit', id)}
+                    className="flex items-center px-6 py-4 focus:text-indigo-700 focus:outline-none"
+                  >
+                    {contact
+                      ? contact.first_name + ' ' + contact.last_name
+                      : ''}{' '}
+                    {Date.parse(fim) <= Date.parse(dataActual) ? ' (Terminado) ' : ''} {Date.parse(fim) == Date.parse(dataAmanha) ? ' (Termina amanhã) ' : ''}
+                    {deleted_at && (
+                      <Icon
+                        name="trash"
+                        className="flex-shrink-0 w-3 h-3 ml-2 text-gray-400 fill-current"
+                      />
+                    )}
+                  </InertiaLink>
+                </td>
+                <td className="border-t">
+                  <InertiaLink
+                    tabIndex="1"
+                    className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                    href={route('pagamentos.edit', id)}
+                  >
+                    {contact ? contact.cantina : ''}
+                  </InertiaLink>
+                </td>
+                <td className="border-t">
+                  <InertiaLink
+                    tabIndex="-1"
+                    href={route('pagamentos.edit', id)}
+                    className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                  >
+                    {inicio}
+                  </InertiaLink>
+                </td>
+                <td className="border-t">
+                  <InertiaLink
+                    tabIndex="-1"
+                    href={route('pagamentos.edit', id)}
+                    className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                  >
+                    {fim}
+                  </InertiaLink>
+                </td>
+                <td className="w-px border-t">
+                  <InertiaLink
+                    tabIndex="-1"
+                    href={route('pagamentos.edit', id)}
+                    className="flex items-center px-4 focus:outline-none"
+                  >
+                    <Icon
+                      name="cheveron-right"
+                      className="block w-6 h-6 text-gray-400 fill-current"
+                    />
+                  </InertiaLink>
+                </td>
+              </tr>
+            ))}
+            {data.length === 0 && (
+              <tr>
+                <td className="px-6 py-4 border-t" colSpan="4">
+                  Nenhum pagamento encontrado.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <Pagination links={links} />
+    </div>
+  );
+};
+
+Index.layout = page => <Layout title="Pagamentos" children={page} />;
+
+export default Index;
