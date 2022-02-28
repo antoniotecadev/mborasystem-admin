974a9c59984a971e9e8719703b4ca5f0130265cb
António Teca <antonioteca@hotmail.com>
2022-02-28 13:11:40 +0100

    responsividade button

--- resources/js/Pages/Notifications/Index.js
+++ resources/js/Pages/Notifications/Index.js
@@ -24,11 +24,13 @@ const Index = () => {
   return (
     <div>
       <h1 className="mb-8 text-3xl font-bold">Notificações de registos ({data.length}) - {tipo == '0' ? 'Não lidas' : tipo == '1' ? 'Lidas' : tipo == '2' ? 'Não atendido' : tipo == '3' ? 'Atendido' : 'Todas' }</h1>
-      <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "4" name = "Todas" color = "btn-indigo"/>
-      <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "0" name = "Não lidas" color = 'btn-danger'/>
-      <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "1" name = "Lidas" color = 'btn-sucess'/>
-      <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "2" name = "❌" color = 'btn-danger'/>
-      <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "3" name = "✔" color = 'btn-sucess'/>
+      <div className="flex flex-wrap">
+        <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "4" name = "Todas" color = "btn-indigo"/>
+        <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "0" name = "Não lidas" color = 'btn-danger'/>
+        <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "1" name = "Lidas" color = 'btn-sucess'/>
+        <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "2" name = "❌" color = 'btn-danger'/>
+        <ButtonQueryNotification handleSubmit = {handleSubmit} processing = {processing} type= "3" name = "✔" color = 'btn-sucess'/>
+      </div>
       <div className="overflow-x-auto bg-white rounded shadow">
         <table className="w-full whitespace-nowrap">
           <thead>
