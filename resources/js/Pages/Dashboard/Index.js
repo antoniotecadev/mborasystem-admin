e5f912760a8b3aa8c94a12adfe0408188b7303f8
António Teca <antonioteca@hotmail.com>
2022-06-11 21:30:58 +0100

    grafico de quantida de parceiro activos e desactivos

--- resources/js/Pages/Dashboard/Index.js
+++ resources/js/Pages/Dashboard/Index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 import Layout from '@/Shared/Layout';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+];
+
+const COLORS = ['#00C49F', '#0088FE'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 const Dashboard = () => {
   return (
     <div>
-      <h1 className="mb-8 text-3xl font-bold">Dashboard</h1>
+      <h1 className="mb-4 text-3xl font-bold">Dashboard</h1>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <CardHeader grafico={<GraficoQuantidadeParceiro />} />
+        <CardHeader grafico={<GraficoQuantidadeParceiro />} />
+        <CardHeader grafico={<GraficoQuantidadeParceiro />} />
+      </div>
       <p className="mb-12 leading-normal">
         Hey there! Welcome to Ping CRM, a demo app designed to help illustrate
         how
@@ -41,3 +67,51 @@ const Dashboard = () => {
 Dashboard.layout = page => <Layout title="Dashboard" children={page} />;
 
 export default Dashboard;
+
+const CardHeader = ({ grafico }) => {
+  return (
+    <div className="justify-center py-8 px-8 max-w bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-bold">
+            PARCEIROS
+          </p>
+          <p className="text-lg font-light">
+            🤝 200.0000
+          </p>
+          <p className="text-xs font-medium">
+            <span style={{ color: '#00C49F' }}>●</span> 150.000 activos
+          </p>
+          <p className="text-xs font-medium">
+            <span style={{ color: '#0088FE' }}>●</span> 50.000 desactivos
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        {grafico}
+      </div>
+    </div>);
+}
+
+class GraficoQuantidadeParceiro extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
+
+  render() {
+    return (
+      <PieChart width={100} height={100} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={data}
+          label={renderCustomizedLabel}
+          outerRadius={50}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    );
+  }
+}
