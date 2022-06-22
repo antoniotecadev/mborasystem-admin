0902aee68a49877ef1b6506c1d63bcf49ced6366
António Teca <antonioteca@hotmail.com>
2022-06-22 22:24:57 +0100

    Routa importada e comentada

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,9 +14,9 @@ use Inertia\Inertia;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/termoscondicoes', function () {
     return Inertia::render('TermosCondicoes/Index');
