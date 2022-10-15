b7c45a5b9eb77494a0931334cdfb8dab063e8ebd
António Teca <antonioteca@hotmail.com>
2022-10-15 20:46:12 +0100

    aumentar tamanho de letra.

--- resources/js/Pages/Dashboard/Index.js
+++ resources/js/Pages/Dashboard/Index.js
@@ -20,7 +20,7 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+    <text x={x} y={y} style={{ fontSize: '12' }} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
       {`${(percent * 100).toFixed(0)}%`}
     </text>
   );
