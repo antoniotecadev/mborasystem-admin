8c2ffb3b0d88bf3e39e5e67402b0f3d23e561247
António Teca <antonioteca@hotmail.com>
2023-01-05 15:30:11 +0100

    criar tabela de produtos do mbora

---
+++ database/migrations/2023_01_05_134258_produtos_mbora.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProdutosMbora extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos_mbora', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('imei', 20)->index();
+            $table->integer('idcategoria')->index();
+            $table->string('nome', 30);
+            $table->integer('preco');
+            $table->integer('quantidade');
+            $table->string('urlImage');
+            $table->string('codigoBarra', 20)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
