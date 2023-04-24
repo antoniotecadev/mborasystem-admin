74b4535c79a6cc74c54085aee1ea9a1545e6f05f
António Teca <antonioteca@hotmail.com>
2023-04-24 12:38:35 +0100

    actualizar a verificação de código de validade

--- app/Http/Controllers/Api/CodeCheckController.php
+++ app/Http/Controllers/Api/CodeCheckController.php
@@ -12,6 +12,8 @@ class CodeCheckController extends BaseController
         try {
             $data = Validator::make($request->all(), [
                 'code' => 'required|string|exists:reset_code_passwords',
+            ], [
+                'exists' => 'Código inserido não é válido.'
             ]);
 
             if($data->fails()):
@@ -21,13 +23,13 @@ class CodeCheckController extends BaseController
 
             // find the code
             $passwordReset = ResetCodePassword::firstWhere('code', $request->code);
-
+            
             // check if it does not expired: the time is one hour
             if ($passwordReset->created_at > now()->addHour()) {
-                $passwordReset->delete();
                 $error['message'] = 'Código inserido não é válido.';
-                return $this->sendError('Código expirado', $error);
+                return $this->sendError('Código expirado', $error);            
             }
+
             $success['message'] = 'Código válido';
             return $this->sendResponse($success, 'Código validado com sucesso');
         } catch (\Throwable $th) {
