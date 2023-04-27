aa0b77e14e46674082ae7602a997d566872ec8d6
António Teca <antonioteca@hotmail.com>
2023-04-27 18:41:52 +0100

    alterar nome de class

--- database/migrations/2023_04_22_112422_create_reset_code_password_table.php
+++ database/migrations/2023_04_22_112422_create_reset_code_password_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateResetCodePasswordphpsTable extends Migration
+class CreateResetCodePasswordTable extends Migration
 {
     /**
      * Run the migrations.
