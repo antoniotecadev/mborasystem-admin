fc7df7e7041b375e2a1a70874c4b3085f568398a
António Teca <antonioteca@hotmail.com>
2023-04-28 15:05:09 +0100

    ordenar consulta com numero de seguidores

--- app/Http/Controllers/Api/EmpresasMboraController.php
+++ app/Http/Controllers/Api/EmpresasMboraController.php
@@ -11,8 +11,8 @@ class EmpresasMboraController extends Controller
     public function index() {
         return DB::table('contacts as ct')
             ->join('provincias as pv', 'pv.id', '=', 'ct.provincia_id')
-            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia')
-            ->orderByDesc('ct.created_at')
+            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia', 'followers_mbora')
+            ->orderByDesc('ct.followers_mbora')
             ->get()->random(10);
     }
 }
