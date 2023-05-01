711e238710651a0e30d22c750bae314cbb89f0eb
António Teca <antonioteca@hotmail.com>
2023-05-01 17:37:52 +0100

    obter numero de seguidores pela subconsulta, para evitar consultas desnecessarias

--- app/Http/Controllers/Api/EmpresasMboraController.php
+++ app/Http/Controllers/Api/EmpresasMboraController.php
@@ -11,7 +11,7 @@ class EmpresasMboraController extends Controller
     public function index() {
         return DB::table('contacts as ct')
             ->join('provincias as pv', 'pv.id', '=', 'ct.provincia_id')
-            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia', 'ct.followers_mbora', 'ct.views_mbora')
+            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia', 'ct.views_mbora')
             ->selectSub(function($query) {
                 $query->selectRaw('count(*)')->from('produtos_mbora')->whereColumn('imei', 'ct.imei');
             }, 'product_number')
@@ -21,7 +21,10 @@ class EmpresasMboraController extends Controller
             ->selectSub(function($query) {
                 $query->selectRaw('estado')->from('seguidores_empresas_mbora')->whereColumn('imei_empresas_mbora', 'ct.imei')->where('id_users_mbora', auth()->user()->id)->limit(1);
             }, 'estado')
-            ->orderByDesc('ct.followers_mbora')
+            ->selectSub(function($query) {
+                $query->selectRaw('count(*)')->from('seguidores_empresas_mbora')->whereColumn('imei_empresas_mbora', 'ct.imei')->where('estado', 1);
+            }, 'followers_number')
+            ->orderByDesc('ct.views_mbora')
             ->get()->random(10);
     }
 
@@ -33,7 +36,7 @@ class EmpresasMboraController extends Controller
                 $query->where('ct.views_mbora', ($isMoreCompany == 'false' ? '>=' : '<') , ($isMoreCompany == 'false' ? 0 : $leastViewed)); // ORDEM DECRESCENTE
             })
             ->join('provincias as pv', 'pv.id', '=', 'ct.provincia_id')
-            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia', 'ct.followers_mbora', 'ct.views_mbora')
+            ->select('ct.id', 'ct.first_name', 'ct.last_name', 'ct.email', 'ct.phone', 'ct.alternative_phone', 'ct.imei', 'ct.empresa', 'ct.district', 'ct.street', 'pv.nome as nomeProvincia', 'ct.views_mbora')
             ->selectSub(function($query) {
                 $query->selectRaw('count(*)')->from('produtos_mbora')->whereColumn('imei', 'ct.imei');
             }, 'product_number')
@@ -43,6 +46,9 @@ class EmpresasMboraController extends Controller
             ->selectSub(function($query) {
                 $query->selectRaw('estado')->from('seguidores_empresas_mbora')->whereColumn('imei_empresas_mbora', 'ct.imei')->where('id_users_mbora', auth()->user()->id)->limit(1);
             }, 'estado')
+            ->selectSub(function($query) {
+                $query->selectRaw('count(*)')->from('seguidores_empresas_mbora')->whereColumn('imei_empresas_mbora', 'ct.imei')->where('estado', 1);
+            }, 'followers_number')
             ->orderByDesc('ct.views_mbora')
             ->limit(10)
             ->get();
