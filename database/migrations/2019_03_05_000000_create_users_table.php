735099705c5a35ba152475636a06f414371fcc09
António Teca <antonioteca@hotmail.com>
2023-07-24 22:46:09 +0100

    adicionar nullable na migracao para permitir valor nulo de telephone.

--- database/migrations/2019_03_05_000000_create_users_table.php
+++ database/migrations/2019_03_05_000000_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
             $table->integer('account_id')->index();
             $table->string('first_name', 25);
             $table->string('last_name', 25);
-            $table->string('telephone', 20)->unique();
+            $table->string('telephone', 20)->unique()->nullable();
             $table->string('email', 50)->unique();
             $table->string('imei_contact', 20)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
