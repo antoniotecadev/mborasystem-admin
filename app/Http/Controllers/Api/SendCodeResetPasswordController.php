e9308d657a514c7d33ea8e737c48689f72aef2bc
António Teca <antonioteca@hotmail.com>
2023-07-31 13:14:28 +0100

    descomentar código que vai definir email que vai receber  código para alterar a password.

--- app/Http/Controllers/Api/SendCodeResetPasswordController.php
+++ app/Http/Controllers/Api/SendCodeResetPasswordController.php
@@ -43,7 +43,7 @@ class SendCodeResetPasswordController extends BaseController
             $codeData = ResetCodePassword::create($request->all());
 
             // Send email to user
-            // Notification::route('mail', ['antonioteca@hotmail.com' => $user->first_name . ' ' . $user->last_name])->notify(new SendCodeResetPasswordNotification($codeData->code));
+            Notification::route('mail', [$request->email => $user->first_name . ' ' . $user->last_name])->notify(new SendCodeResetPasswordNotification($codeData->code));
             DB::commit();
             $success['message'] = 'Código enviado';
             return $this->sendResponse($success, 'Código enviado com sucesso');
